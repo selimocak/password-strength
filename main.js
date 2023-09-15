cc54186b81f6eec1842253122e35d37bd931b6ac
Selim Ocak <selimocakbe@gmail.com>
2023-09-15 10:50:32 +0300

    end

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 var pass = document.getElementById('password')
 var msg = document.getElementById('message')
-var str = document.getElementById('strength')
 
 pass.addEventListener('input', () => {
-  if(pass.value.length > 0){
+  var strength = ''
+
+  if (pass.value.length > 0) {
     msg.style.display = "block"
-  }
-  else {
+  } else {
     msg.style.display = "none"
   }
-  if(pass.value.length < 4){
-    str.innerHTML = "weak"
+
+  if (pass.value.length < 4) {
+    strength = "zayıf"
     pass.style.borderColor = "#ff5925"
     msg.style.color = "#ff5925"
-  }
-  else if(pass.value.length >= 4 && pass.value.length < 8){
-    str.innerHTML = "medium"
+  } else if (pass.value.length >= 4 && pass.value.length < 8) {
+    strength = "orta"
     pass.style.borderColor = "yellow"
     msg.style.color = "yellow"
-  }
-  else if(pass.value.length >= 8){
-    str.innerHTML = "strong"
+  } else if (pass.value.length >= 8) {
+    strength = "güçlü"
     pass.style.borderColor = "#26d730"
-    msg.style.color = "#26d730" 
+    msg.style.color = "#26d730"
   }
-})
 
+  // Mesaj metnini "Şifre " + strength olarak ayarla
+  msg.innerHTML = "Şifre " + strength
+})
